3e446c05d12df3735ef36de301907b3d909c1189
Jonathan Ipe <jonathan@Jonathans-MacBook-Pro.local>
2025-02-16 19:16:05 -0800

    added ucg_file ground truth data

--- lit-ras/datamodules/ucg2cg.py
+++ lit-ras/datamodules/ucg2cg.py
@@ -11,31 +11,35 @@ from torch.utils.data import Dataset
 from torch_scatter import scatter
 
 class UCG2CGDataset(Dataset):
-    def __init__(self, cg_files: List[str], ucg_index_file: str, scale: float = 1.0):
+    def __init__(self, cg_files: List[str], ucg_files: List[str], ucg_index_file: str, scale: float = 1.0):
         super().__init__()
         self.cg_files       = cg_files
+        self.ucg_files      = ucg_files
         self.ucg_index_file = ucg_index_file
         self.scale          = scale
 
         # Determine global-to-local indexing for map-style dataset implementation
         self.global_idx:   List[int]        = []  # global indices
         self.local_idx:    List[int]        = []  # local indices
-        self.cg_pos_trajs: List[np.ndarray] = []  # CG position trajectories: 
+        self.cg_pos_trajs: List[np.ndarray] = []  # CG position trajectories:
+        self.ucg_pos_trajs: List[np.ndarray] = []
+
+        # Load CG trajectories
         for i, fname in enumerate(cg_files):
             data = np.load(fname, allow_pickle=True)
             cg_pos_traj = data['protein_positions']
             traj_len = len(cg_pos_traj)
-            # cg_pos_traj = np.load(fname, mmap_mode='r')
-            # traj_len = len(cg_pos_traj)
             self.global_idx.extend([i]*traj_len)
             self.local_idx.extend(list(range(traj_len)))
             self.cg_pos_trajs.append(cg_pos_traj)
 
+        # Load UCG trajectories
+        for i, fname in enumerate(ucg_files):
+            data = np.load(fname, allow_pickle=True)
+            ucg_pos_traj = data['positions_ucg']
+            self.ucg_pos_trajs.append(ucg_pos_traj)
+
         # Read UCG index file
-        # self.ucg_idx = []
-        # with open(ucg_index_file, 'r') as f:
-        #     for line in f:
-        #         self.ucg_idx.append([int(s) for s in line.rstrip().split(',')])
         data = np.load(ucg_index_file, allow_pickle=True)
         self.ucg_idx = data["indices_per_cluster"]
 
@@ -66,7 +70,8 @@ class UCG2CGDataset(Dataset):
         cg_pos = cg_pos[self.ucg_flat_idx]
 
         # Compute UCG pos
-        ucg_pos = scatter(src=cg_pos, index=self.scatter_idx, dim=0, reduce='mean')
+        ucg_pos = self.ucg_pos_trajs[global_idx][local_idx] * self.scale
+        ucg_pos = torch.tensor(ucg_pos, dtype=torch.float)
 
         # Compute CG displacements
         cg_disp = cg_pos - ucg_pos[self.scatter_idx]
@@ -85,8 +90,10 @@ class UCG2CGDataset(Dataset):
 
 cg_files = ["/Users/jonathan/Documents/LLNLMLBackmapping/sample-data/cg/pfpatch_000000000138.npz", "/Users/jonathan/Documents/LLNLMLBackmapping/sample-data/cg/pfpatch_000000000214.npz", "/Users/jonathan/Documents/LLNLMLBackmapping/sample-data/cg/pfpatch_000000000272.npz"]
 ucg_idx_file = "/Users/jonathan/Documents/LLNLMLBackmapping/sample-data/cg/all_indices_per_cluster.npz"
-dataset = UCG2CGDataset(cg_files=cg_files, ucg_index_file=ucg_idx_file)
+ucg_files = ["/Users/jonathan/Documents/LLNLMLBackmapping/sample-data/ucg/pfpatch_000000000138_ucg.npz", "/Users/jonathan/Documents/LLNLMLBackmapping/sample-data/ucg/pfpatch_000000000214_ucg.npz", "/Users/jonathan/Documents/LLNLMLBackmapping/sample-data/ucg/pfpatch_000000000272_ucg.npz"]
+dataset = UCG2CGDataset(cg_files=cg_files, ucg_files=ucg_files, ucg_index_file=ucg_idx_file)
 
-print(dataset)
-print(len(dataset))
-print(dataset[0]['ucg_pos'])
+# print(dataset)
+# print(len(dataset))
+# print(dataset[0]['ucg_pos'])
+# print(dataset[0]['cg_pos'])
