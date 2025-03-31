cab9c3184cf7c3b5f72bba1a061c8449a922a83d
Jonathan Ipe <ipejonathan@gmail.com>
2025-03-31 16:37:21 -0700

    bug fix

--- lit_ras/model_analysis.py
+++ lit_ras/model_analysis.py
@@ -77,7 +77,7 @@ def generate_rmsd_plot(num_steps, file_name):
     plot_rmsds(all_rmsds, title=f"Validation RMSD Distribution - {num_steps} steps", filename=file_name)
 
 
-generate_rmsd_plot(num_steps=1, file_name="val_rmsds_1.png")
+generate_rmsd_plot(num_steps=500, file_name="val_rmsds_500.png")
 print("job finished")
 # generate_rmsd_plot(num_steps=100, file_name="val_rmsds_100.png")
 # generate_rmsd_plot(num_steps=500, file_name="val_rmsds_500.png")
