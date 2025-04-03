8ccfdd55d98f0381701b28a81a1b513d4e540802
Jonathan Ipe <ipejonathan@gmail.com>
2025-04-03 10:49:43 -0700

    npz viewer

--- open_npz.py
+++ open_npz.py
@@ -1,14 +1,22 @@
 from numpy import load
 import numpy as np
 
-# data = load('./sample-data/cg/all_indices_per_cluster.npz', allow_pickle=True)
-data = load("/Users/jonathan/Documents/LLNLMLBackmapping/sample-data/ucg/pfpatch_000000000138_ucg.npz", allow_pickle=True)
+data = load('./sample-data/cg/all_indices_per_cluster.npz', allow_pickle=True)
+# data = load("/Users/jonathan/Documents/LLNLMLBackmapping/sample-data/ucg/pfpatch_000000000138_ucg.npz", allow_pickle=True)
 lst = data.files
 print(lst)
 # print(data["aligned_anchors_BB_index"])
-# print(len(data["indices_per_cluster"]))
-print(data["positions_ucg"][0])
-
+print(data["indices_per_cluster"].shape)
+print(data['indices_per_cluster'])
+# print(data["positions_ucg"][0])
+sum = 0
+max = 0
+for item in data['indices_per_cluster']:
+    if len(item) > max:
+        max = len(item)
+    sum += len(item)
+print(max)
+print(sum)
 
 
 # print(data[lst[0]].shape)
