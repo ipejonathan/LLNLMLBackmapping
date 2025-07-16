2958609ab2607255cce4f5af379fe88f025c3bc3
Jonathan Ipe <ipejonathan@gmail.com>
2025-07-16 04:11:07 -0500

    inference job scripts for all inference i hope this works lmao

--- jobs/run_chunk.sh
+++ jobs/run_chunk.sh
@@ -17,36 +17,17 @@ source /usr/workspace/ipe1/anaconda/bin/activate
 module load cuda/11.8.0
 conda activate opence-1.9.1
 
-####################### Distributed Setup #######################
-
-# Record which nodes are used
-lrun -T1 hostname
-
-# Set MASTER_ADDR to the hostname of the first node
-firsthost=$(lrun -N1 -n1 hostname)
-echo "First host: $firsthost"
-
-export MASTER_ADDR=$firsthost
-export MASTER_PORT=23456 # Arbitrary unused port
 
 ####################### Launch Multi-GPU Inference #######################
 
-while IFS= read -r ucg_file || [[ -n "$ucg_file" ]]; do
+while IFS= read -r ucg_file; do
     fname=$(basename "$ucg_file" .npz)
     out_file="${OUTPUT_DIR}/${fname}_cg.npy"
     echo "Processing $ucg_file"
 
-    lrun -T4 --gpubind=off python "$SCRIPT" \
+    python "$SCRIPT" \
         --ucg-file "$ucg_file" \
         --out-dir "$out_file" \
         --cg-generator "$CG_GENERATOR"
 
-    if [[ $? -ne 0 ]]; then
-        echo "❌ Error processing $ucg_file" >&2
-    else
-        echo "✅ Successfully processed $ucg_file"
-    fi
-
-    echo
-    
 done < "$CHUNK_FILE"
