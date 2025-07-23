f8cbf81c63d9c41576498bd4ea1b8b7debd65bf1
Jonathan Ipe <ipejonathan@gmail.com>
2025-07-22 18:23:03 -0700

    running a lot of jobs

--- jobs/run_chunk.sh
+++ jobs/run_chunk.sh
@@ -1,54 +1,22 @@
-#!/bin/bash
-#BSUB -W 12:00
-#BSUB -G cancer
-#BSUB -q pbatch
-#BSUB -nnodes 1
-#BSUB -J inf_chunk
-#BSUB -o inf_chunk-%J.log
-#BSUB -alloc_flags ipisolate
-
 CHUNK_ID=$1  # Passed as an argument
 CHUNK_FILE="/p/gpfs1/ipe1/LLNLMLBackmapping/inference_chunks/chunk_${CHUNK_ID}.txt"
-CG_GENERATOR="/p/gpfs1/ipe1/LLNLMLBackmapping/lit_logs/ras-raf-test/version_4/checkpoints/epoch=1800-step=585325.ckpt"
-SCRIPT="/p/gpfs1/ipe1/LLNLMLBackmapping/lit_ras/inference.py"
-OUTPUT_DIR="/p/gpfs1/ipe1/LLNLMLBackmapping/cg_outputs"
 
-source /usr/workspace/ipe1/anaconda/bin/activate
-module load cuda/11.8.0
-conda activate opence-1.9.1
-
-####################### Distributed Setup #######################
-
-# Record which nodes are used
-lrun -T1 hostname
-
-# Set MASTER_ADDR to the hostname of the first node
-firsthost=$(lrun -N1 -n1 hostname)
-echo "First host: $firsthost"
-
-export MASTER_ADDR=$firsthost
-export MASTER_PORT=23456 # Arbitrary unused port
 
 ####################### Launch Multi-GPU Inference #######################
 
 while IFS= read -r ucg_file; do
-    fname=$(basename "$ucg_file" .npz)
-    out_file="${OUTPUT_DIR}/${fname}_cg.npy"
-    echo "Processing $ucg_file"
+    # fname=$(basename "$ucg_file" .npz)
+    # out_file="${OUTPUT_DIR}/${fname}_cg.npy"
+    # echo "Processing $ucg_file"
 
-    (
-        lrun -T4 --gpubind=off python "$SCRIPT" \
-            --ucg-file "$ucg_file" \
-            --out-dir "$out_file" \
-            --cg-generator "$CG_GENERATOR"
+    bsub < single_inference.sh "$ucg_file"
 
-        status=$?
-        if [[ $status -ne 0 ]]; then
-            echo "Failed: $ucg_file"
-        else
-            echo "Done: $ucg_file"
-        fi
-    )
+    status=$?
+    if [[ $status -ne 0 ]]; then
+        echo "Failed: $ucg_file"
+    else
+        echo "Done: $ucg_file"
+    fi
 
     echo
 
---
+++ jobs/single_inference.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#BSUB -nnodes 1
+#BSUB -W 00:10
+#BSUB -G cancer
+#BSUB -q pbatch
+#BSUB -J infer_single
+#BSUB -o infer_single-%J.log
+#BSUB -alloc_flags ipisolate
+
+source /usr/workspace/ipe1/anaconda/bin/activate
+module load cuda/11.8.0
+conda activate opence-1.9.1
+
+UCG_FILE=$1
+FNAME=$(basename "$UCG_FILE" .npz)
+OUT_FILE="/p/gpfs1/ipe1/LLNLMLBackmapping/cg_outputs/${FNAME}_cg.npy"
+SCRIPT="/p/gpfs1/ipe1/LLNLMLBackmapping/lit_ras/inference.py"
+CKPT="/p/gpfs1/ipe1/LLNLMLBackmapping/lit_logs/ras-raf-test/version_4/checkpoints/epoch=1800-step=585325.ckpt"
+
+
+####################### Distributed Setup #######################
+
+# Record which nodes are used
+lrun -T1 hostname
+
+# Set MASTER_ADDR to the hostname of the first node
+firsthost=$(lrun -N1 -n1 hostname)
+echo "First host: $firsthost"
+
+export MASTER_ADDR=$firsthost
+export MASTER_PORT=23456 # Arbitrary unused port
+
+
+
+lrun -T4 --gpubind=off python "$SCRIPT" \
+    --ucg-file "$UCG_FILE" \
+    --out-dir "$OUT_FILE" \
+    --cg-generator "$CKPT"
